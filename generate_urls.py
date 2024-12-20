94564aa7ebcd03c96c47d144d1db98cc4aa81b47
Jagadeesh-Rachapudi <110329542+Jagadeesh-Rachapudi@users.noreply.github.com>
2024-12-20 22:25:34 +0530

    Create generate_urls.py

---
+++ generate_urls.py
@@ -0,0 +1,57 @@
+import argparse
+from pathlib import Path
+
+from voxpopuli import LANGUAGES, LANGUAGES_V2, YEARS, DOWNLOAD_BASE_URL
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--subset", "-s", type=str, required=True,
+        choices=["400k", "100k", "10k", "asr"] + LANGUAGES + LANGUAGES_V2,
+        help="Data subset to generate URLs for"
+    )
+    parser.add_argument(
+        "--output", "-o", type=str, default="urls.txt",
+        help="Output file to save the URLs"
+    )
+    return parser.parse_args()
+
+def generate_urls(args):
+    if args.subset in LANGUAGES_V2:
+        languages = [args.subset.split("_")[0]]
+        years = YEARS + [f"{y}_2" for y in YEARS]
+    elif args.subset in LANGUAGES:
+        languages = [args.subset]
+        years = YEARS
+    else:
+        languages = {
+            "400k": LANGUAGES,
+            "100k": LANGUAGES,
+            "10k": LANGUAGES,
+            "asr": ["original"]
+        }.get(args.subset, None)
+        years = {
+            "400k": YEARS + [f"{y}_2" for y in YEARS],
+            "100k": YEARS,
+            "10k": [2019, 2020],
+            "asr": YEARS
+        }.get(args.subset, None)
+
+    url_list = []
+    for l in languages:
+        for y in years:
+            url_list.append(f"{DOWNLOAD_BASE_URL}/audios/{l}_{y}.tar")
+
+    # Save the URLs to the specified output file
+    with open(args.output, "w") as f:
+        for url in url_list:
+            f.write(url + "\n")
+
+    print(f"{len(url_list)} URLs saved to {args.output}")
+
+def main():
+    args = get_args()
+    generate_urls(args)
+
+if __name__ == '__main__':
+    main()
