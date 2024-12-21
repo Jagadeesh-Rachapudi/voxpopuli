7b0d4c7164905cbf3cb8c8edb2844713dd194191
Jagadeesh Rachapudi <rachapudijaagdeesh9580@gmail.com>
2024-12-21 10:41:29 +0530

    upadted

--- generate_urls.py
+++ generate_urls.py
@@ -1,41 +1,12 @@
-import argparse
 from pathlib import Path
-
 from voxpopuli import LANGUAGES, LANGUAGES_V2, YEARS, DOWNLOAD_BASE_URL
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--subset", "-s", type=str, required=True,
-        choices=["400k", "100k", "10k", "asr"] + LANGUAGES + LANGUAGES_V2,
-        help="Data subset to generate URLs for"
-    )
-    parser.add_argument(
-        "--output", "-o", type=str, default="urls.txt",
-        help="Output file to save the filenames and URLs"
-    )
-    return parser.parse_args()
+def generate_urls():
+    subset = "10k"
+    output = "10k_urls_full"
 
-def generate_urls(args):
-    if args.subset in LANGUAGES_V2:
-        languages = [args.subset.split("_")[0]]
-        years = YEARS + [f"{y}_2" for y in YEARS]
-    elif args.subset in LANGUAGES:
-        languages = [args.subset]
-        years = YEARS
-    else:
-        languages = {
-            "400k": LANGUAGES,
-            "100k": LANGUAGES,
-            "10k": LANGUAGES,
-            "asr": ["original"]
-        }.get(args.subset, None)
-        years = {
-            "400k": YEARS + [f"{y}_2" for y in YEARS],
-            "100k": YEARS,
-            "10k": [2019, 2020],
-            "asr": YEARS
-        }.get(args.subset, None)
+    languages = LANGUAGES
+    years = [2019, 2020]  # Specific to the 10k subset
 
     # Generate the list of filenames and URLs
     url_list = []
@@ -46,16 +17,14 @@ def generate_urls(args):
             url_list.append((filename, url))
 
     # Save the filenames and URLs to the specified output file
-    with open(args.output, "w") as f:
+    with open(output, "w") as f:
         for filename, url in url_list:
             f.write(f"{filename}: {url}\n")
 
-    print(f"{len(url_list)} filenames and URLs saved to {args.output}")
+    print(f"{len(url_list)} filenames and URLs saved to {output}")
 
 def main():
-    args = get_args()
-    generate_urls(args)
-    print(args)
+    generate_urls()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
